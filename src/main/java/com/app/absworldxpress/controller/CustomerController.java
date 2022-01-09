f6ee64611ed90006a7d1d6684cd25e48a2f9c2f6
Injamamul Haque Sonet <ihsonnet@gmail.com>
2022-01-09 20:47:02 +0600

    customer controller mapping

--- src/main/java/com/app/absworldxpress/controller/CustomerController.java
+++ src/main/java/com/app/absworldxpress/controller/CustomerController.java
@@ -6,15 +6,15 @@ import com.app.absworldxpress.services.CustomerService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/customers/")
 public class CustomerController {
     private CustomerService customerService;
 
+    @GetMapping
     public ResponseEntity<ApiResponse<CustomerListResponse>> getUsersList(@RequestHeader(name = "Authorization") String token,
                                                                           @RequestParam(required = false) String customerName,
                                                                           String customerPhoneNo, String createdBy,
