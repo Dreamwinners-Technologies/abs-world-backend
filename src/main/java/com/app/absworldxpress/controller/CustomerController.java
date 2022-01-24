7a23733f47d2164b329b8e0110a396639ef9e5b7
Injamamul Haque Sonet <ihsonnet@gmail.com>
2022-01-24 12:20:44 +0600

    handled cors

--- src/main/java/com/app/absworldxpress/controller/CustomerController.java
+++ src/main/java/com/app/absworldxpress/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/customers/")
